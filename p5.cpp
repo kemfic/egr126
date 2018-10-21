c141a5c5af1a09d8c6b6efde28cc4122956fcef1
kemfic1 <19ficici.kemal@gmail.com>
2018-10-21 15:20:13 -0400

    added project 5, haven't done it yet because i love to procrastinate

---
+++ p5.cpp
@@ -0,0 +1,26 @@
+/*
+2018, Kemal Ficici
+  Project 5, EGR-126
+
+    Program A.
+      Develop a program containing 4 separate C++ functions to perform the following
+      on a one-dimensional integer array LIST of dimension N:
+
+        1. Determine (i.e. “return”) the highest value in LIST.
+        2. Determine (i.e. “return”) the lowest value in LIST.
+        3. Determine (i.e. “return”) the sum of all the items in LIST.
+        4. Determine (i.e. “return”) the average of the values in LIST after 
+           eliminating the lowest and the highest values. This function should
+           invoke some or all of the other 3 functions.
+
+   
+    Program B.
+      Develop a C++ program that will add the corresponding elements of two 
+      2-dimensional arrays X and Y to produce the 2-dimensional array Z.  
+      Arrays X, Y, Z have the same dimension [M] [N] (i.e. M rows and N columns).   
+
+    NOTE: In the above programs, you will need to have the user input the dimensions of the respective arrays before execution of the functions.
+
+
+*/
+
