f3e77012297402123833e04ba25e71c715588981
kemfic1 <19ficici.kemal@gmail.com>
2018-11-08 16:03:33 -0500

    fixed headers

---
+++ p5/p5_b.cpp
@@ -0,0 +1,13 @@
+/*
+2018, Kemal Ficici
+  Project 5, EGR-126
+
+    Program B.
+      Develop a C++ program that will add the corresponding elements of two 
+      2-dimensional arrays X and Y to produce the 2-dimensional array Z.  
+      Arrays X, Y, Z have the same dimension [M] [N] (i.e. M rows and N columns).   
+
+    NOTE: In the above programs, you will need to have the user input the dimensions of the respective arrays before execution of the functions.
+
+
+*/
